9dba9d025a9b5dd5aa78be014ac7f7011f41b1de
TuratinZhii <zhiixiiang@163.com>
2022-05-02 07:25:06 -0700

    Fibonacci_20220502

--- Geek_society_activity/Task_submit/尹静安/20220427.cpp
+++ Geek_society_activity/Task_submit/尹静安/20220427.cpp
@@ -1,32 +1,37 @@
 //
 
-void fabonacci(int,int);
+#include<cstdio>
+using namespace std;
+int arr_i[2];   //用于接收输入
+int arr_f[999][999];    //用于形成斐波那契数列
+int fibo(int);
+void print(int,int);    //输出函数，两个变量即目标对象的横、纵坐标
 int main(){
-    FILE *inputNUM;
-    inputNUM=fopen("in.txt","rb");
-    int inputNum;
-    for (int i=1;i<=2;i++){
-        fscanf(inputNUM,"%d",&inputNum);    //小写尾缀是变量，大写尾缀是指针
-        arr_2[i]=inputNum;
+    FILE *INPUT;
+    INPUT=fopen("in.txt","rb");
+    int temp_1,temp_2;
+    for (int i=1; i<=2; i++){
+        fscanf(INPUT,"%d",&temp_1);
+        arr_i[i]=temp_1;
     }
-    arr_1[1][1]=1;
-    fabonacci(arr_2[0],arr_2[1]);
-    return 0;
+    temp_2=fibo(arr_i[1]);
+    print(temp_2,arr_i[2]);
+    return 0;   
 }
 
-void fabonacci(int Hlmt,int Olmt){  //"H"为横行，"O"为纵列
-    int Htemp=2,Otemp=1;
-    while (Htemp<=Hlmt && Otemp<=Olmt){
-        if (Htemp==Otemp){
-            arr_1[Htemp][Otemp]=1;
-            Htemp+=1;
-            Otemp=1;
-        }
-        else{
-            arr_1[Htemp][1]=1;
-            arr_1[Htemp][Otemp]=arr_1[Htemp-1][Otemp-1]+arr_1[Htemp-1][Otemp];
-            Otemp+=1;
-        }
+int fibo(int h_std){
+    int retu_H=2;
+    arr_f[1][1]=1;
+    for (int h=2; h<=h_std-1; h++){
+        arr_f[h][1]=1;
+        arr_f[h][h]=1;
+        retu_H++;
+        for (int o=2; o<=h-1; o++)
+        arr_f[h][o]=arr_f[h-1][o-1] + arr_f[h-1][o];
     }
-    printf("The result is:\n %d",arr_1[Htemp][Otemp]);
+    return retu_H;  //返回目标对象的行序数
+}
+
+void print(int a,int b){
+    printf("The result is:\n %d",arr_f[a][b]);
 }
