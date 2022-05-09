02c3b8cf53bff3a133ef3c3c086a645de8254eb6
zeerre <zeerre98988@gmail.com>
2022-05-09 08:06:34 +0800

    Add New interface

---
+++ Geek_society_activity/Temp/function.h
@@ -0,0 +1,15 @@
+//*************************************************************************
+//* File Name: function.h
+//* Author:GeekBear
+//* Mail:zeerre98988@gmail.com 
+//* Created And Modified Time: ====2022-05-09  07-45-33====
+//*************************************************************************
+// 自定义函数的头文件
+// 请将函数定义写在此文件中。
+#ifndef _FUNCTION_H
+#define _FUNCTION_H
+
+//例如：
+int example(int);
+
+#endif
---
+++ Geek_society_activity/Temp/main.cpp
@@ -0,0 +1,17 @@
+//*************************************************************************
+//* File Name: main.cpp
+//* Author:GeekBear
+//* Mail:zeerre98988@gmail.com 
+//* Created And Modified Time: ====2022-05-09  07-46-03====
+//*************************************************************************
+
+#include <iostream>
+#include"function.h"
+
+using namespace std;
+int main() {
+    int x;
+    std::cin>>x;
+    std::cout<<example(x)<<std::endl;
+    return 0;
+}
